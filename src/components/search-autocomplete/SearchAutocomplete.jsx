0a1d005d7e1d26224fbade93dfa7f048f0317645
Khvede93 <gio.khvede93@gmail.com>
2024-09-19 23:23:14 +0200

    temp add states for data, error, loading

--- src/components/search-autocomplete/SearchAutocomplete.jsx
+++ src/components/search-autocomplete/SearchAutocomplete.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
 
 export const SearchAutocomplete = () => {
-  return <div className='test'>SearchAutocomplete</div>;
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState(null);
+
+  async function fetchUsers() {
+    try {
+    } catch (error) {
+      setError(error);
+    } finally {
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className='container'>
+      <input type='text' name='search-users' placeholder='Search Users ...' />
+    </div>
+  );
 };
